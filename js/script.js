05b293a684fcf0f8ad4e403c767281191ae91563
kdh9585 <kdh9585@gmail.com>
2022-05-31 01:52:39 +0900

    modal 창

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
+    // 모달창
+    let modal_close = $('.modal-close');
+    let modal = $('.modal-wrap');
 
+    modal_close.click(function(){
+        modal.hide();
+    });
+
+    let modal_bt = $('.modal-bt');
+    modal_bt.click(function(){
+        modal.show();
+    });
     // 주메뉴 모션 적용
     let nav = $('.nav');
     let headerMain = $('.header-main');
